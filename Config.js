1e98f26c8610c1dbbcb77abbed9dc30866b52ac3
jahnhane1980 <jahnhane@gmail.com>
2026-04-03 19:38:43 +0200

    Add global configuration settings in Config.js

---
+++ Config.js
@@ -0,0 +1,8 @@
+// Config.js - Globale Konfigurationseinstellungen für die App
+
+export const Config = {
+  GOOGLE_API: {
+    URL: "https://generativelanguage.googleapis.com/v1beta/models/gemini-3-flash:generateContent",
+    KEY: "Unkown",
+  },
+};
